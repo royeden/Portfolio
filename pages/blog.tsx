dedb2966fea5518305402f5205a5f1b906330326
royeden <roye@worcket.com>
2020-06-06 19:36:35 -0300

    Wrapped in page component

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,12 @@
 import PageHeader from '../components/PageHeader';
+import PageContainer from '../components/PageContainer';
 
 function Blog(): JSX.Element {
   return (
-    <>
+    <PageContainer>
       <PageHeader page="Blog" title="Blog" />
       Blog
-    </>
+    </PageContainer>
   );
 }
 
